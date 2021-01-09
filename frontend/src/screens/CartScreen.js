331819be1a2cbdd76c8787f7b10b55e421097feb
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-01-09 17:08:50 -0500

    mobileview & UI part

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,11 @@
+ import './CartScreen.css'
+
+function CartScreen() {
+    return (
+        <div>
+            CartScreen
+        </div>
+    )
+}
+
+export default CartScreen
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+ import './ProductScreen.css'
+
+function ProductScreen() {
+    return (
+        <div>
+            ProductScreen
+        </div>
+    )
+}
+
+export default ProductScreen
